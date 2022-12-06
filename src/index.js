1f39f312a84e5b07b754adcc29ee5f3ce06814c9
David <david.owuor@student.moringaschool.com>
2022-12-06 18:18:44 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const init = () => {
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+   const input = document.querySelector('#searchByID')
+   console.log(input.value)
+
+   fetch(`http://localhost:3000/movies/${input.value}`)
+  .then(res=>{
+    return res.json()
+  })
+  .then(data=>{
+    const title = document.querySelector('#title')
+    const summary = document.querySelector('#summary')
+    title.textContent = data.title
+    summary.textContent = data.summary
+   // console.log(data);
+  })
+  })
   
 }
 
